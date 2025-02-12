00d729686664d3e11dc83f1ea63e81838d8c7547
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2025-02-12 09:55:43 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: '⚡ Velocidad web | Artxeweb',
   description: 'Comprueba la velocidad de tu web de forma gratis, rápida y sencilla.',
+  icons: {
+    icon: '/favicon.svg', // Define el favicon correctamente
+  },
 };
 
 export default function RootLayout({
@@ -16,11 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
